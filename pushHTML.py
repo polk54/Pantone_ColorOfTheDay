e5d6aec8d016433d21916c8c9e5e2debe73350fa
Parul <polk54@gmail.com>
2019-04-07 18:52:04 -0400

    update

--- pushHTML.py
+++ pushHTML.py
@@ -4,19 +4,21 @@
 ### DEFINE FUNCTION:
 def push():
     from git import Repo
-    import os
-
-    repo_dir = "/Users/parulagarwal/Documents/Code/GitHub/Pantone_ColorOfTheDay"
-    repo = Repo(repo_dir)
-    file_list = [
-        "index.html",
-        "Colors.csv"
-    ]
-    commit_message = 'Added new color'
-    repo.index.add(file_list)
-    repo.index.commit(commit_message)
-    origin = repo.remote('origin')
-    origin.push()
+    try:
+        repo_dir = "/Users/parulagarwal/Documents/Code/GitHub/Pantone_ColorOfTheDay"
+        repo = Repo(repo_dir)
+        file_list = [
+            "index.html",
+            "Colors.csv"
+        ]
+        commit_message = 'Added new color'
+        repo.index.add(file_list)
+        repo.index.commit(commit_message)
+        origin = repo.remote('origin')
+        origin.push()
+        print("Push succeeded")
+    except:
+        print("Something went wrong")
 
 ## RUN FUNCTION:
 push()
